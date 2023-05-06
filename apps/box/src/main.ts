048e8bedf58057a41fa3373d70c91502cd42f538
Tyler Beck <t.beck1990@gmail.com>
2023-05-06 00:59:58 -0400

    sockets

--- apps/box/src/main.ts
+++ apps/box/src/main.ts
@@ -13,6 +13,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
